a092d9c5f76a529482c8f83876051d1fd634cc65
Riley Marek <riley.marek@wsu.edu>
2025-04-18 21:16:02 -0700

    added test cases

--- src/test/java/com/in28minutes/junit/helper/StringHelperTestComplete.java
+++ src/test/java/com/in28minutes/junit/helper/StringHelperTestComplete.java
@@ -16,29 +16,47 @@ public class StringHelperTestComplete {
 		helper = new StringHelper();
 	}
 
+ // Test cases for truncateAInFirst2Positions
+    @Test
+    public void testTruncateAInFirst2Positions_AACD() {
+        assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
+    }
 
-	@Test
-	public void testTruncateAInFirst2Positions_AinFirst2Positions() {
-		assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
-	}
+    @Test
+    public void testTruncateAInFirst2Positions_ACD() {
+        assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
+    }
 
-	@Test
-	public void testTruncateAInFirst2Positions_AinFirstPosition() {
-		assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
-	}
+    @Test
+    public void testTruncateAInFirst2Positions_CDEF() {
+        assertEquals("CDEF", helper.truncateAInFirst2Positions("CDEF"));
+    }
 
-	// ABCD => false, ABAB => true, AB => true, A => false
-	@Test
-	public void testAreFirstAndLastTwoCharactersTheSame_BasicNegativeScenario() {
-		assertFalse( 
-				helper.areFirstAndLastTwoCharactersTheSame("ABCD"));
-	}
+    @Test
+    public void testTruncateAInFirst2Positions_CDAA() {
+        assertEquals("CDAA", helper.truncateAInFirst2Positions("CDAA"));
+    }
 
-	@Test
-	public void testAreFirstAndLastTwoCharactersTheSame_BasicPositiveScenario() {
-		assertTrue( 
-				helper.areFirstAndLastTwoCharactersTheSame("ABAB"));
-	}
+    // Test cases for areFirstAndLastTwoCharactersTheSame
+    @Test
+    public void testAreFirstAndLastTwoCharactersTheSame_ABCD() {
+        assertFalse(helper.areFirstAndLastTwoCharactersTheSame("ABCD"));
+    }
+
+    @Test
+    public void testAreFirstAndLastTwoCharactersTheSame_ABAB() {
+        assertTrue(helper.areFirstAndLastTwoCharactersTheSame("ABAB"));
+    }
+
+    @Test
+    public void testAreFirstAndLastTwoCharactersTheSame_AB() {
+        assertTrue(helper.areFirstAndLastTwoCharactersTheSame("AB"));
+    }
+
+    @Test
+    public void testAreFirstAndLastTwoCharactersTheSame_A() {
+        assertFalse(helper.areFirstAndLastTwoCharactersTheSame("A"));
+    }
 
 
 }
